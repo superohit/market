fafd4e43cfa189780dd5a2bf0610406ec4344387
Rohit Kumar <59915418+superohit@users.noreply.github.com>
2023-02-03 01:22:02 +0530

    Update models.py

--- markett/models.py
+++ markett/models.py
@@ -2,18 +2,21 @@ from markett import db, login_manager
 from markett import bcrypt
 from flask_login import UserMixin
 
+# getting user id
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+# User table
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer, nullable=False, default=1000)
-    items = db.relationship('Item', backref='owned_user', lazy=True)
-
+    items = db.relationship('Item', backref='owned_user', lazy=True) # represents relaionship between user and items
+    
+    # to show precise amount balance
     @property
     def prettier_budget(self):
         if len(str(self.budget)) >= 4:
@@ -37,6 +40,7 @@ class User(db.Model, UserMixin):
     def can_sell(self, item_obj):
         return item_obj in self.items
 
+# items table
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
